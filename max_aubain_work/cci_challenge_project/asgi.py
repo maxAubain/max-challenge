e150919f4c829d83a27ebeb5257dffab4d5c75e9
Max Aubain <maxaubain@protonmail.com>
2020-02-28 11:54:29 -0500

    Renames 'framework' project folder to 'cci_challenge_project'

--- max_aubain_work/cci_challenge_project/asgi.py
+++ max_aubain_work/cci_challenge_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for framework project.
+ASGI config for cci_challenge_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'framework.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cci_challenge_project.settings')
 
 application = get_asgi_application()
--- max_aubain_work/cci_challenge_project/wsgi.py
+++ max_aubain_work/cci_challenge_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for framework project.
+WSGI config for cci_challenge_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'framework.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cci_challenge_project.settings')
 
 application = get_wsgi_application()
