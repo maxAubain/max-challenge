09f2208f71bc99a36017057a1b210c0d4b76c6c6
Max Aubain <maxaubain@protonmail.com>
2020-02-28 13:13:30 -0500

    ..._project/urls, ...app/views, ...app/urls: adds index http request, routes, and permissions; .gitignore: adds cache file folders

--- max_aubain_work/map_app/views.py
+++ max_aubain_work/map_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Home page
+def index(request):
+  return HttpResponse("This is the map_app index page.")
