2734b1732fc29c777b37f68c9ef7adff53d50ac4
Max Aubain <maxaubain@protonmail.com>
2020-03-01 13:20:02 -0500

    scripts.js: binds popup event on each shapefile layer that shows select layer and feature information

--- max_aubain_work/map_app/static/map_app/scripts.js
+++ max_aubain_work/map_app/static/map_app/scripts.js
@@ -1,12 +1,15 @@
+// Generate map with view parameters
 let maxMap = L.map('mapid', {
   center: [35.530435614394, -115.785923098035],
   zoom: 6
 });
 
+// Add baselayer to map
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(maxMap);
 
+// Add overlay to map, aligned by bounding box
 L.imageOverlay('/static/map_app/Gopherus_agassizii_connectivity.png',
   [
     [32.887232724714, -118.888883749725],
@@ -18,37 +21,17 @@ L.imageOverlay('/static/map_app/Gopherus_agassizii_connectivity.png',
     zIndex: 1,
   }).addTo(maxMap)
 
-shp('/static/map_app/ClarkGain.zip').then(function (geojson) {
-  L.geoJSON(geojson).addTo(maxMap)
-});
+// Extract and condition shapefile data
+shp('/static/map_app/ClarkGain.zip').then(function (geoJson) {
 
-/*
-GeoJSON data extracted from the .tif file using rasterio CLI => `rio bounds Gopherus_agassizii_connectivity.tif --indent 2`
-reference: https://rasterio.readthedocs.io/en/latest/cli.html
-const geoJsonData = {
-  "bbox": [
-    -118.888883749725,
-    32.887232724714,
-    -112.682962446345,
-    38.173638504074
-  ],
-  "geometry": {
-    "coordinates": [
-      [
-        [-118.888883749725, 32.887232724714],
-        [-112.682962446345, 32.887232724714],
-        [-112.682962446345, 38.173638504074],
-        [-118.888883749725, 38.173638504074],
-        [-118.888883749725, 32.887232724714]
-      ]
-    ],
-    "type": "Polygon"
-  },
-  "properties": {
-    "filename": "Gopherus_agassizii_connectivity.tif",
-    "id": "0",
-    "title": "Gopherus_agassizii_connectivity.tif"
-  },
-  "type": "Feature"
-}
-*/
+  // Add popup onClick of each layer 
+  L.geoJSON(geoJson, {
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+        `<b><u>Feature properties</u></b> <br/>
+        <b>Name</b>: ${feature.properties.Name || "(no name assigned)"} <br/> 
+        <b>Designation</b>: ${feature.properties.designatio || "(no designation assigned)"}`
+      );
+    }
+  }).addTo(maxMap)
+});
